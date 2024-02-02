1229f70c67877b548ca4dfe0fabe3a28f36c4663
vikashgaud <er.vikashgaid9919@gmail.com>
2024-02-02 19:34:10 +0530

    add new file and write a new  code

---
+++ JavaBasic3.java
@@ -0,0 +1,16 @@
+package CodingSkillTech;
+
+import java.util.*;
+
+// user define input Print Sum of two values a+b=10+5=15
+
+public class JavaBasic3 {
+    public static void main(String args[]){
+        System.out.println("Enter Value any number");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int Sum = a + b;
+        System.out.println("Answer ="+ Sum);
+    }
+}
