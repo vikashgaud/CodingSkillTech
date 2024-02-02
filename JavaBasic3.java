c51279e01a23510a1329a972fbc5ba5545f9e3c7
vikashgaud <er.vikashgaid9919@gmail.com>
2024-02-02 21:20:53 +0530

    Include code circle of area

--- JavaBasic3.java
+++ JavaBasic3.java
@@ -8,9 +8,14 @@ public class JavaBasic3 {
     public static void main(String args[]){
         System.out.println("Enter Value any number");
         Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
-        int b = input.nextInt();
-        int Sum = a + b;
-        System.out.println("Answer ="+ Sum);
+        // int a = input.nextInt();
+        // int b = input.nextInt();
+        // int Sum = a + b;
+        // System.out.println("Answer ="+ Sum);
+
+        // circle of area
+        float radious = input.nextFloat();
+        float area = 3.14f * radious * radious;
+        System.out.println(area);
     }
 }
