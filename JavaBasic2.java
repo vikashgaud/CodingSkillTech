f68a1743e850c3126170809f1705c052311606b5
vikashgaud <er.vikashgaid9919@gmail.com>
2024-02-02 17:49:20 +0530

    add new file and write code

--- JavaBasic2.java
+++ JavaBasic2.java
@@ -1,5 +1,12 @@
 package CodingSkillTech;
 
+// Print Sum of two values a+b=10+5=15
+
 public class JavaBasic2 {
-    
+    public static void main(String args[]){
+        int a=10;
+        int b=5;
+        int sum = a+b;
+        System.out.println(sum);
+    }
 }
