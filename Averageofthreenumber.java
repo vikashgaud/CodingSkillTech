582827a0e5b67d750a4a2afb307ba766ed624384
vikashgaud <er.vikashgaid9919@gmail.com>
2024-02-15 15:57:10 +0530

    Write a code in file

--- Averageofthreenumber.java
+++ Averageofthreenumber.java
@@ -1,5 +1,18 @@
 package CodingSkillTech;
 
+// Question1:   In a program, input 3 numbers : A, B and C. You have to output the average of these 3 numbers.
+            // (Hint : Average of N numbers is sum of those numbers divided by N)
+
+import java.util.*;
+
 public class Averageofthreenumber {
-    
+    public static void main(String args []){
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        int Average = (a+b+c)/2;
+        System.out.println("Answer of Average Number = "+ Average);
+    }
+
 }
