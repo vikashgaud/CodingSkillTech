bbf8a9c4897735de59eb40637ade3426c9a000df
vikashgaud <er.vikashgaid9919@gmail.com>
2024-02-15 16:04:00 +0530

    Write a code in file

--- Averageofthreenumber.java
+++ Averageofthreenumber.java
@@ -2,6 +2,9 @@ package CodingSkillTech;
 
 // Question1:   In a program, input 3 numbers : A, B and C. You have to output the average of these 3 numbers.
             // (Hint : Average of N numbers is sum of those numbers divided by N)
+            
+            // प्रश्न 1: एक प्रोग्राम में, 3 नंबर इनपुट करें: A, B और C। आपको इन 3 नंबरों का औसत आउटपुट करना होगा।  
+            // (संकेत: एन नंबरों का औसत N से विभाजित उन नंबरों का योग है)
 
 import java.util.*;
 
