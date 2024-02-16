7a8234138099c5c8d1b6a496f6809d22663fbfab
vikashgaud <er.vikashgaid9919@gmail.com>
2024-02-16 12:56:56 +0530

    New code

---
+++ OperatorOuestion2.java
@@ -0,0 +1,13 @@
+package CodingSkillTech;
+
+public class OperatorOuestion2 {
+    public static void main(String args[]){
+        int x = 200, y = 50, z = 100;
+        if(x>y && y>z){
+            System.out.println("Hello");
+        } 
+        if(z>y && z<x){
+            System.out.println("Java");
+        }
+    }
+}
