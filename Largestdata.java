78574cce4ad4999e3b07fd61a5f59970184f8349
vikashgaud <er.vikashgaid9919@gmail.com>
2024-02-16 12:21:03 +0530

    Include a new code

---
+++ Largestdata.java
@@ -0,0 +1,22 @@
+package CodingSkillTech;
+
+//Question 4:What will be the type of result in thefollowing Java code?
+// प्रश्न 4: निम्नलिखित जावा कोड में परिणाम का प्रकार क्या होगा?
+                // byte b = 4;
+                // char c = "a";
+                // short s = 512;
+                // int i = 1000;
+                // float f = 3.14f;
+                // double d = 99.9954;
+                // result = (f*b)+(i%c)-(d*s);
+
+//(Hint : Look at the largest data type among these)
+// (संकेत: इनमें से सबसे बड़े डेटा प्रकार को देखें)
+
+public class Largestdata {
+    public static void main(String args []){
+        // In the mentioned code, the result variable will be of double type because of type conversion.
+        // उल्लिखित कोड में, प्रकार रूपांतरण के कारण परिणाम चर दोहरे प्रकार का होगा।
+    }
+    
+}
